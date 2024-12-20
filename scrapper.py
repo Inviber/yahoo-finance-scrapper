f294803b3c8616cdbbdadfff3915eb429155e367
Inviber <bkelley4600@gmail.com>
2024-12-19 19:53:02 -0600

    gets the main stock ticker data

--- scrapper.py
+++ scrapper.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -23,6 +27,23 @@ driver.get(url)
 
 # scrapping logic
 print("Scrapping site")
+stock = {}
+
+regular_market_price = driver.find_element(By.CSS_SELECTOR, f'[data-symbol="{ticker_symbol}"][data-field="regularMarketPrice"]').text
+regular_market_change = driver.find_element(By.CSS_SELECTOR, f'[data-symbol="{ticker_symbol}"][data-field="regularMarketChange"]').text
+regular_market_change_percent = driver.find_element(By.CSS_SELECTOR, f'[data-symbol="{ticker_symbol}"][data-field="regularMarketChangePercent"]').text.replace('(', '').replace(')', '')
+post_market_price = driver.find_element(By.CSS_SELECTOR, f'[data-symbol="{ticker_symbol}"][data-field="postMarketPrice"]').text
+post_market_change = driver.find_element(By.CSS_SELECTOR, f'[data-symbol="{ticker_symbol}"][data-field="postMarketChange"]').text
+post_market_change_percent = driver.find_element(By.CSS_SELECTOR, f'[data-symbol="{ticker_symbol}"][data-field="postMarketChangePercent"]').text.replace('(', '').replace(')', '')
+
+stock['regular_market_price'] = regular_market_price
+stock['regular_market_change'] = regular_market_change
+stock['regular_market_change_percent'] = regular_market_change_percent
+stock['post_market_price'] = post_market_price
+stock['post_market_change'] = post_market_change
+stock['post_market_change_percent'] = post_market_change_percent
+
+print(stock)
 
 # close the browser and free up the resources
 driver.quit()
